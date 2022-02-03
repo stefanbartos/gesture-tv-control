e9f4a25f322fd4c594906ab41de71c615a1f2d60
Jan Palicka <jan.palicka@24i.com>
2022-02-03 09:01:36 +0100

    fix: fixing QR reader

--- webapp/src/QR-reader.tsx
+++ webapp/src/QR-reader.tsx
@@ -1,25 +1,49 @@
 import React, { useState } from 'react';
 import { QrReader } from '@blackbox-vision/react-qr-reader';
+import { useNavigate } from "react-router-dom";
 
 const QRScreen = () => {
+  const navigate = useNavigate();
   const [data, setData] = useState('No result');
 
-  var constraints = {
-    facingMode: 'environment'
-  };
+  const processData = (data: string): void => {
+
+    try {
+      const dataObject = JSON.parse(data);
+      const { type, uuid } = dataObject;
+
+      setData(data);
+
+      if (type && type === 'TV') {
+        if (uuid) {
+          localStorage.setItem('tvID', uuid);
+          alert(`TV ID ${uuid} was loaded`);
+        }
+      }
+
+      navigate('/');
+    } catch (e) {
+      console.error(e);
+      alert('Error occurred');
+    }
+  }
 
   return (
     <>
       <QrReader
         onResult={(result, error) => {
-          setData(JSON.parse(result))
+          if (result) {
+            processData(result.getText());
+          }
 
           if (!!error) {
-            console.info(error);
+            console.error(error);
           }
         }}
-        style={{ width: '100%' }}
-        constraints={constraints}
+        videoStyle={{ width: '100%' }}
+        constraints={{
+          facingMode: 'environment'
+        }}
       />
       <p>{data}</p>
     </>
